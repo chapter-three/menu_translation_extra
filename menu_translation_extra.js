96692a8c3250d5902ceadcdab747dc284b7c06f1
Philip Boden <philip.boden@gmail.com>
2014-09-02 22:29:27 -0700

    Javascript clean up (keep table head when hiding rows). Added full language name to menu item row for better identification.

--- menu_translation_extra.js
+++ menu_translation_extra.js
@@ -4,30 +4,33 @@
     attach: function (context, settings) {
       $(document).ready(function(){;
         $('#no_menu_items_to_display').hide();
-        $('#menu-overview tr').hide();
+        $('#menu-overview tbody tr.menu-enabled').hide();
         if ($('#menu-overview-form-language-selector').val() == 'all') {
           $('#menu-overview tr').show();
         }
         else {
           $('.lang-' + $('#menu-overview-form-language-selector').val()).show();
         }
-        if($('#menu-overview tr').children(':visible').length == 0) {
+        if($('#menu-overview tr.menu-enabled').children(':visible').length == 0) {
           $('#no_menu_items_to_display').show();
+          $('#menu-overview thead tr').hide();
         }
       });
       $('#menu-overview-form-language-selector').change(function(){;
-        $('#menu-overview tr').hide();
+        $('#menu-overview tr.menu-enabled').hide();
         if ($(this).val() == 'all') {
           $('#menu-overview tr').show();
         }
         else {
           $('.lang-' + $(this).val()).show();
         }
-        if ($('#menu-overview tr').children(':visible').length == 0) {
+        if ($('#menu-overview tr.menu-enabled').children(':visible').length == 0) {
           $('#no_menu_items_to_display').show();
+          $('#menu-overview thead tr').hide();
         }
         else {
           $('#no_menu_items_to_display').hide();
+          $('#menu-overview thead tr').show();
         }
       });
     }
